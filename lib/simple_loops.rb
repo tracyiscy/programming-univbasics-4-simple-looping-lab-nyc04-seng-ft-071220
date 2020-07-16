2a728f11e118fdca9813706704aee829e732f6f9
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-16 14:02:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,14 +6,29 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times
-
+def loop_message_n_times(message,number)
+  count=0
+  while count<number) do
+    puts message
+    count+=1
+  end
 end
 
-def return_string_array
-
+def return_string_array(array)
+  count=0
+  newarray=Arrat.new
+  while count<array.length do
+    newarray.push(array[count])
+    count+=1
+  end
+  newarray
 end
 
-def output_array
+def output_array(array)
+  count=0
+  while count<array.length do
+    puts array[count]
+    count+=1
+  end
 
 end
