4d6694e630d55e46cb0e4c75e3161343cfa807fc
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-16 14:06:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,7 +18,7 @@ def return_string_array(array)
   count=0
   newarray=[]
   while count<array.length do
-    newarray.push(array[count].to_i)
+    newarray.push(array[count].to_s)
     count+=1
   end
   newarray
